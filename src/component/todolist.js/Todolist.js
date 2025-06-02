ed5c14278f1c0d30d3b5d684ed0bb63de2ff95e1
Shashank HL <Shashank.hl@inventechinfo.com>
2025-06-02 20:38:57 +0530

    changes

--- src/component/todolist.js/Todolist.js
+++ src/component/todolist.js/Todolist.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function Todolist() {
+const Todolist = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
